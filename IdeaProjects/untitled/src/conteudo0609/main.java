e1e9ab4ab6bd22f381b550bb621e498d78532c3a
Marcus Vinicius <marcusviniciusferrari@gmail.com>
2021-09-06 21:23:03 -0300

    teste1

---
+++ IdeaProjects/untitled/src/conteudo0609/main.java
@@ -0,0 +1,16 @@
+package conteudo0609;
+
+public class main {
+
+    public static void main(String[] args) {
+        int [] diasSemana = { 1, 2, 3, 4, 5, 6, 7 };
+
+                System.out.println(" dia da semana ");
+
+        for  (int i = 0; < diasSemana.length; i++) {
+
+            System.out.println(diasSemana[i]);
+        }
+
+    }
+}
